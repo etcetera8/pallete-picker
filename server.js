a0734171d31566b95db8d0698de63c760bfb6f80
parker lindley <fugazi8@gmail.com>
2018-03-12 14:02:58 -0600

    Serving static files through express

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+app.set('port', process.env.PORT || 3000);
+app.use(bodyParser.json());
+app.use(express.static('public'));
+app.locals.title = 'Pallete Picker';
+
+// app.get('/', (request, response) => {
+//   response.s
+// })
+
+app.listen(app.get('port'), () => {
+  console.log(`${app.locals.title} server running on port 3000`); 
+})
