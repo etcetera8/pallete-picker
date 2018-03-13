967ade32e7343d1a6a71bee3993e626ebac94ae1
parker lindley <fugazi8@gmail.com>
2018-03-12 18:09:13 -0600

    Add hexcodes to screen on click

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,9 +1,15 @@
 const generate = $('#generate');
 
+window.onload = () => {
+  generatePalette();
+}
+
 const generatePalette = () => {
   const palleteArray = document.querySelectorAll('.unlocked');
   palleteArray.forEach(color => {
-    $(`#${color.id}`).css("background-color", `${generateHex()}`)
+    const hexCode = generateHex();
+    $(`#${color.id}`).css("background-color", `${hexCode}`)
+    $(`#${color.id} span:first-child`).text(hexCode);
   });
 }
 
@@ -17,6 +23,6 @@ window.onkeydown = function (e) {
 generate.click(() => generatePalette());
 
 const generateHex = () => {
-  return '#' + (Math.random() * 0xFFFFFF << 0).toString(16);
+  return '#' + (Math.random() * 0xFFFFFF << 0).toString(16).toUpperCase();
 }
 
