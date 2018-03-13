de93b9799e55179187bd33047797bcc5e99df512
parker lindley <fugazi8@gmail.com>
2018-03-12 18:08:20 -0600

    Breakout palette generator from and event listener

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,11 +1,20 @@
 const generate = $('#generate');
 
-generate.click(() => {
+const generatePalette = () => {
   const palleteArray = document.querySelectorAll('.unlocked');
   palleteArray.forEach(color => {
-    $(`#${color.id}`).css("background-color", `${generateHex()}`)    
-  })  
-})
+    $(`#${color.id}`).css("background-color", `${generateHex()}`)
+  });
+}
+
+window.onkeydown = function (e) {
+  if (e.keyCode == 32 && e.target == document.body) {
+    e.preventDefault();
+    generatePalette();
+  }
+};  
+
+generate.click(() => generatePalette());
 
 const generateHex = () => {
   return '#' + (Math.random() * 0xFFFFFF << 0).toString(16);
