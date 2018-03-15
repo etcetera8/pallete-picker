74ffb5de782f87a6ce7a2cd42cadd6cff5264269
parker lindley <fugazi8@gmail.com>
2018-03-15 14:26:43 -0600

    Client route testing complete

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -6,7 +6,23 @@ const server = require('../server');
 chai.use(chaiHttp);
 
 describe('Client Routes', () => {
+  it('should return the homepage', () => {
+    return chai.request(server)
+    .get('/', (req, res) => {
+      response.should.have.status(200)
+    })
+  })
 
+  it('should return 404 for page that does not exist', () => {
+    return chai.request(server)
+    .get('/banana')
+    .then(response => {
+      response.should.have.status(404)
+    })
+    .catch(err => {
+      throw err;
+    })
+  })
 });
 
 describe('API Routes', () => {
