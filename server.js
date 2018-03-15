e5b1035be2478e22d636d0840483730c849d89e8
parker lindley <fugazi8@gmail.com>
2018-03-14 19:30:27 -0600

    Clean server file of app.locals

--- server.js
+++ server.js
@@ -10,47 +10,6 @@ app.set('port', process.env.PORT || 3000);
 app.use(bodyParser.json());
 app.use(express.static('public'));
 app.locals.title = 'Palette Picker';
-app.locals.projects = [
-  {
-    "id": "1",
-    "project_name": "Project 1"
-  },
-  {
-    "id": "2",
-    "project_name": "Project 2"
-  },
-  {
-    "id": "3",
-    "project_name": "Project 3"
-  }
-];
-
-app.locals.palettes = [
-  { 
-    "id": "1234",
-    "palette_name": "not fun stuff",
-    "hex_codes": ["#000", "#111", "#fff", "#555", "#321"],
-    "project_key": "1"
-  },
-  { 
-    "id": "12345",
-    "palette_name": "fun stuff",
-    "hex_codes": ["#ac468b", "#fe55de", "#26e425", "#fc133e", "#f25d90"],
-    "project_key": "1"
-  },
-  {
-    "id": "234",
-    "palette_name": "sequence",
-    "hex_codes": ["#bc568b", "#ce75de", "#36b425", "#fc133e", "#114346"],
-    "project_key": "2"
-  },
-  {
-    "id": "2345",
-    "palette_name": "greeenies",
-    "hex_codes": ["#ce75ae", "#ce75de", "#36b425", "#fc133e", "#00a"],
-    "project_key": "3"
-  },
-];
 
 app.get('/api/v1/projects', (request, response) => {
   database('projects').select()
@@ -144,40 +103,3 @@ app.get('/api/v1/projects/:id/palettes', (request, response) => {
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} server running on port 3000`); 
 })
-
-// app.delete('/api/v1/palettes/:id', (request, response) => {
-//   const { id } = request.params;
-//   const { palette_id } = request.headers
-//   const newPalettes = app.locals.palettes.filter( (palette, index) => palette.id != palette_id)
-//   app.locals.palettes = newPalettes;
-//   response.status(202).json(newPalette);
-// })
-
-// app.get('/api/v1/palette/:id/', (request, response) => {
-//   const { id } = request.params;
-// })
-
-// app.get('/api/v1/palettes/:id/', (request, response) => {
-//   const { id } = request.params;
-  
-//   const palette = app.locals.palettes.filter( palette => palette.project_key === id);
-//   if (palette) {
-//     response.status(200).json(palette)
-//   } else {
-//     response.sendStatus(404)
-//   }
-// });
-// app.post('/api/v1/projects', (request, response) => {
-//   const id = globalId;
-//   globalId += 1;
-
-//   const { project_name } = request.body;
-//   const project = { id, project_name }
-//   app.locals.projects.push(project);
-//   response.status(201).json(project)
-// })
-
-// app.post('/api/v1/palettes', (request, response) => {
-//   app.locals.palettes.push(request.body);
-//   response.status(201).json(request.body);
-// })
