2ca6901654c8bdc4effd8bdbbf8a7eee15803b3a
parker lindley <fugazi8@gmail.com>
2018-03-20 10:27:58 -0600

    make changes

--- server.js
+++ server.js
@@ -5,8 +5,6 @@ const app = express();
 const bodyParser = require('body-parser');
 
 const environment = process.env.NODE_ENV || 'development';
-
-
 const configuration = require('./knexfile')[environment];
 const database = require('knex')(configuration);
 
